98a0d3c4c86b7d68b76f2082c106064b6dc9de13
LinkinStar <linkinstar@foxmail.com>
2020-10-09 17:22:15 +0800

    fmt: update build path

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-go build -o baileys ./src/baileys/cmd/
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o baileys.exe ./src/baileys/cmd/
+go build -o baileys ./cmd/baileys
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o baileys.exe ./cmd/baileys
 
 tar -zcvf ./baileys-v1.0.0-darwin-amd64.tar.gz ./baileys-v1.0.0-darwin-amd64
 tar -zcvf ./baileys-v1.0.0-darwin-windows.tar.gz ./baileys-v1.0.0-windows-amd64
